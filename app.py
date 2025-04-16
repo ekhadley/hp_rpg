a6adfe62a4bff377c888b88093a4dd66097c56a6
Ethan Hadley <ekhadley@gmail.com>
2025-04-16 09:26:19 -0500

    app rewrite done basically

--- app.py
+++ app.py
@@ -31,16 +31,15 @@ def select_story(data):
     ], default_kwargs={"current_story": story_name})
 
     asst = Assistant(
-        #model_name = "claude-3-7-sonnet-20250219",
+        model_name = "claude-3-7-sonnet-20250219",
         #model_name = "claude-3-haiku-20240307",
-        model_name = "gpt-4o-mini",
+        #model_name = "gpt-4o-mini",
         #model_name = "gpt-4o-",
         #model_name = "gpt-4.1",
         toolbox = story_tb,
         callback_handler = WebCallbackHandler(socket),
         instructions = "Your job is to operate as an interactive narrator for the world of Harry Potter, enhanced with a dice-based RPG ruleset inspired by D&D. This system blends immersive storytelling with mechanics to create a dynamic experience. Your role is to weave authentic, atmospheric descriptions and dialogue in J.K. Rowling's style while integrating RPG elements like character stats, dice rolls, and a Magical Stamina system for spellcasting. The simulation maintains chronological consistency, character authenticity, and player agency. You will be given 3 instruction files. One containing the ruleset of the game, one containing all spells and abilities, and one containing instructions for correct narration and storytelling. You should follow these guides precisely to ensure a consistent and engaging experience for the player."
     )
-
 
     history_exists = asst.load(story_history_path)
     if history_exists:
